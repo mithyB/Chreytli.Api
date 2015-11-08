a3e7babdd82d495a51f49afb40a89d10bea8d784
Michael <michael.buholzer@outlook.com>
2015-11-08 18:11:55 +0100

    preparations for future features

---
+++ Chreytli.Api/Controllers/RiotApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chreytli.Api.Controllers
+{
+    public class RiotApiController : Controller
+    {
+        // GET: RiotApi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetMatchHistory()
+        {
+            return "Kappa";
+        }
+    }
+}
